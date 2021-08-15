586936a0d70930029fdb15907bb8ffa20461ee52
Shoji Makino <pitang1965@gmail.com>
2021-08-15 16:07:52 +0900

    Add code to fetch API from front end

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,6 +1,6 @@
-exports.handler = async function(event, context) {
+exports.handler = async function (event, context) {
   return {
-      statusCode: 200,
-      body: JSON.stringify({message: "Hello World"})
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Hello World' }),
   };
-}
+};
--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,58 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react';
+import useSWR from 'swr';
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const fetcher = (url) => axios.get(url).then((res) => res.data);
+
+const useGreeting = () => {
+  const { data, error } = useSWR('/.netlify/functions/hello', fetcher);
+
+  console.log (data);
+
+  return {
+    greeting: data?.message,
+    isLoading: !error && !data,
+    isError: error,
+  };
+};
+
+const App = () => {
+  const { greeting, isLoading, isError } = useGreeting();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        {isLoading && <p>読込中...</p>}
+        {isError && <p>読み込みエラー</p>}
+        {greeting && <p>{greeting}</p>}
         <p>
           Edit <code>App.jsx</code> and save to test HMR updates.
         </p>
         <p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Learn React
           </a>
           {' | '}
           <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='App-link'
+            href='https://vitejs.dev/guide/features.html'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Vite Docs
           </a>
         </p>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
